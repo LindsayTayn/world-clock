498d6851223ee461422f98bd45fc81b99b05bc65
LindsayTayenne <154006361+LindsayTayn@users.noreply.github.com>
2024-01-15 20:49:49 +0100

    Added real time and dates of the locations with Javascript

---
+++ javascript/index.js
@@ -0,0 +1,42 @@
+function updateTime() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //Lisbon
+  let lisbonElement = document.querySelector("#lisbon");
+  let lisbonDateElement = lisbonElement.querySelector(".date");
+  let lisbonTimeElement = lisbonElement.querySelector(".time");
+  let lisbonTime = moment().tz("Europe/Lisbon");
+
+  lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+  lisbonTimeElement.innerHTML = lisbonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
